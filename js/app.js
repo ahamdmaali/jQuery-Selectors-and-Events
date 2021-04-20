3cd441a73f42940c904fa285073197fb7e11db79
ahamdmaali <ahmad.maali95@outlook.com>
2021-04-20 17:03:42 +0300

    completing projest functionality and styling

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 'use stricr'
 
+let hornArr=[];
 function Horn(horn){
     this.image_url=horn.image_url;
     this.title=horn.title;
     this.description=horn.description;
     this.keyword=horn.keyword;
     this.horns=horn.horns;
+    hornArr.push(this)
 };
 
 Horn.prototype.render = function (){
+    let hornClone = $('.photo-template').clone();
     let option=document.createElement('option');
     option.textContent=this.keyword;
-    option.setAttribute('id',this.keyword);
-    $('select').append(option);
-    let hornClone = $('#photo-template').clone();
-    $('main').append(hornClone);
+    option.setAttribute('class',this.keyword);
+   $('select').append(option);
+   
     hornClone.find('img').attr('src', this.image_url);
     hornClone.find('h2').text(this.title);
     hornClone.find('p').text(this.description);
-    hornClone.attr('id', this.title);
+    hornClone.attr('class', this.keyword);
+    $('main').append(hornClone);
   };
-
+ 
 Horn.readJson=()=>{
 const ajaxsettings= {
     method:'get',
@@ -31,10 +34,16 @@ $.ajax('./data/page-1.json',ajaxsettings)
 .then(data=>{
     data.forEach(item => {
         let horn = new Horn(item);
-        console.log(horn);
-        horn.render()
+        horn.render(); 
     });
 });
 };
+$('select').on('change',function(event){
+    event.preventDefault();
+   let select=$(this).children('option:selected').val();
+   $('main').children().addClass('hide');
+   $(`.${select}`).removeClass('hide')
+  
+})
 
 $(()=>Horn.readJson());
